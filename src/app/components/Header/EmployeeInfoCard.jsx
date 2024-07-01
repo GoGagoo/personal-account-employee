628f664870e20c03bf6ade8f85e110ec92a7f9ca
Gagik <gagika57@gmail.com>
2024-07-01 19:27:49 +0500

    📱 Adaptive design for Header's component

--- src/app/components/Header/EmployeeInfoCard.jsx
+++ src/app/components/Header/EmployeeInfoCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const EmployeeInfoCard = () => {
 	return (
 		<div>
-			<div className='flex items-center'>
+			<div className='flex flex-col sm:flex-row items-center'>
 				<div>
 					<Image
 						className='rounded-full'
@@ -13,21 +13,21 @@ const EmployeeInfoCard = () => {
 						alt=''
 					/>
 				</div>
-				<div className='whitespace-normal font-bold text-5xl ml-12 leading-[56px]'>
+				<div className='whitespace-normal font-bold text-center sm:text-left text-2xl sm:text-5xl mt-4 sm:mt-0 sm:ml-12 leading-8 sm:leading-[56px]'>
 					Иванов
 					<br />
 					Иван Иванович
 				</div>
 			</div>
-			<div>
-				<div className='flex flex-col gap-y-4'>
-					<div className='ml-52 mt-2'>Junior UI/UX designer</div>
-					<div className='ml-52'>
-						Россия, Красноярск <span className='text-zinc-500'>• 14:03</span>{' '}
-					</div>
+			<div className='flex flex-col items-center sm:items-start mt-4 sm:mt-0'>
+				<div className='text-center sm:text-left sm:ml-52 mt-2'>
+					Junior UI/UX designer
+				</div>
+				<div className='text-center sm:text-left sm:ml-52 mt-4'>
+					Россия, Красноярск <span className='text-zinc-500'>• 14:03</span>
 				</div>
 			</div>
-			<div className='flex items-center mt-14 text-[14px] group uppercase font-bold mr-4'>
+			<div className='flex items-center justify-center sm:justify-start mt-6 sm:mt-[52px] text-[14px] group uppercase font-bold sm:mr-4'>
 				<div className='flex-none border-b-2 transition group-hover:border-zinc-200 border-zinc-700 ml-2 text-zinc-500 hover:text-zinc-200 cursor-pointer'>
 					<div>Основная информация</div>
 				</div>
--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import EmployeeInfoCard from './EmployeeInfoCard'
 
 const Header = () => {
 	return (
-		<div className='bg-zinc-800 mt-6 pl-10 pt-10 min-h-96 rounded-xl'>
+		<div className='bg-zinc-800 mt-6 sm:pl-10 pt-10 pb-7 sm:min-h-80 rounded-xl'>
 			<EmployeeInfoCard />
 		</div>
 	)
